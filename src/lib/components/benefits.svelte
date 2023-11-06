168d3490410a374111fda09aaaa0a6dd4371f91f
navhits <naveen.sunilkumar@freshworks.com>
2023-11-06 13:29:05 +0530

    Merge upstream changes

--- src/lib/components/benefits.svelte
+++ src/lib/components/benefits.svelte
@@ -3,9 +3,18 @@
 </script>
 
 <div
-	class="text-[min(12vw,80px)] leading-[1] tracking-tighter text-center max-w-xl font-bold text-transparent bg-clip-text bg-gradient-to-b from-black to-gray-700 h-auto"
+	class="text-[min(12vw,80px)] leading-[1] tracking-tighter text-center max-w-3xl font-bold text-transparent bg-clip-text bg-gradient-to-b from-black to-gray-700 h-auto"
 >
-	Why attend DevFest 2023? Because you get <span
+	Why attend DevFest 2023?
+</div>
+<div
+	class="text-[min(12vw,80px)] tracking-tighter text-center max-w-3xl text-transparent bg-clip-text bg-gradient-to-b from-black to-gray-700 h-auto"
+>
+Because you get
+</div>
+<div
+	class="text-[min(12vw,80px)] leading-[1] tracking-tighter text-center max-w-xl font-bold text-transparent bg-clip-text bg-gradient-to-b from-black to-gray-700 h-auto"
+> <span
 		class="text-green-500 inline-flex flex-col h-[calc(theme(fontSize.3xl)*theme(lineHeight.loose))] md:h-[calc(theme(fontSize.4xl)*theme(lineHeight.loose))] overflow-hidden"
 	>
 		<ul
