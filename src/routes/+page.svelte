058965d74553968ce7e6219b76d859d275a2d25c
navhits <naveen.sunilkumar@freshworks.com>
2023-11-07 10:19:24 +0530

    Update socials and seo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,31 @@
 	import Memories from '$lib/components/sections/memories.svelte';
 </script>
 
+<svelte:head>
+  <title>GDG Chennai - Devfest 2023</title>
+  <meta name="description" content="GDG Chennai - Devfest 2023" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="theme-color" content="#ffffff" />
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:site" content="@gdgchennai" />
+  <meta name="twitter:creator" content="@gdgchennai" />
+  <meta name="twitter:title" content="GDG Chennai - Devfest 2023" />
+  <meta name="twitter:description" content="GDG Chennai - Devfest 2023" />
+  <meta
+    name="twitter:image"
+    content="https://res.cloudinary.com/ddnnw8qn5/image/upload/v1699332500/devfest23/site/ujnsjqwmmq8rw1fvclmh.webp"
+  />
+  <meta property="og:type" content="website" />
+  <meta property="og:title" content="GDG Chennai - Devfest 2023" />
+  <meta property="og:description" content="GDG Chennai - Devfest 2023" />
+  <meta property="og:site_name" content="GDG Chennai - Devfest 2023" />
+  <meta property="og:url" content="https://devfest.gdgchennai.in/" />
+  <meta
+    property="og:image"
+    content="https://res.cloudinary.com/ddnnw8qn5/image/upload/v1699332500/devfest23/site/ujnsjqwmmq8rw1fvclmh.webp"
+  />
+</svelte:head>
+
 <main
 	class="flex-col flex w-full place-items-center min-h-screen items-center justify-center gap-32"
 >
--- src/routes/sponsorship.pdf/+page.svelte
+++ src/routes/sponsorship.pdf/+page.svelte
@@ -2,6 +2,31 @@
 	import Sponsorship from '$lib/assets/sponsorship.pdf';
 </script>
 
+<svelte:head>
+  <title>GDG Chennai - Devfest 2023</title>
+  <meta name="description" content="GDG Chennai - Devfest 2023" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="theme-color" content="#ffffff" />
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:site" content="@gdgchennai" />
+  <meta name="twitter:creator" content="@gdgchennai" />
+  <meta name="twitter:title" content="GDG Chennai - Devfest 2023" />
+  <meta name="twitter:description" content="GDG Chennai - Devfest 2023" />
+  <meta
+    name="twitter:image"
+    content="https://res.cloudinary.com/ddnnw8qn5/image/upload/v1699332500/devfest23/site/ujnsjqwmmq8rw1fvclmh.webp"
+  />
+  <meta property="og:type" content="website" />
+  <meta property="og:title" content="GDG Chennai - Devfest 2023" />
+  <meta property="og:description" content="GDG Chennai - Devfest 2023" />
+  <meta property="og:site_name" content="GDG Chennai - Devfest 2023" />
+  <meta property="og:url" content="https://devfest.gdgchennai.in/" />
+  <meta
+    property="og:image"
+    content="https://res.cloudinary.com/ddnnw8qn5/image/upload/v1699332500/devfest23/site/ujnsjqwmmq8rw1fvclmh.webp"
+  />
+</svelte:head>
+
 <div class="flex flex-col w-full items-center justify-center h-screen">
 	<iframe
 		title="Sponsorship Prospectus"
