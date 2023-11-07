7919b84b6cb6b0c10a8b24524e3e0ccd9d0f0ecc
Spikey Sanju <spikeysanju98@gmail.com>
2023-11-07 12:15:59 +0530

    add custom breakpoint text animation

--- src/lib/components/benefits.svelte
+++ src/lib/components/benefits.svelte
@@ -6,10 +6,10 @@
 	class="text-[min(12vw,80px)] leading-[1] tracking-tighter text-center max-w-xl font-bold text-transparent bg-clip-text bg-gradient-to-b from-black to-gray-700 h-auto"
 >
 	Why attend DevFest 2023? Because you get <span
-		class="text-green-500 inline-flex flex-col h-16 sm:h-20 overflow-hidden"
+		class="text-green-500 inline-flex flex-col h-[calc(28px*2)] sm:h-[calc(32px*2)] md:h-[calc(34px*2.5)] overflow-hidden"
 	>
 		<ul
-			class="block animate-text-slide-5 text-center leading-tight [&_li]:block -my-4 text-transparent bg-clip-text bg-gradient-to-b from-sky-500 to-purple-600"
+			class="block animate-text-slide-5 text-center leading-tight [&_li]:block -my-[12px] sm:-my-[18px] md:-my-[20px] lg:-my-[20px] text-transparent bg-clip-text bg-gradient-to-b from-sky-500 to-purple-600"
 		>
 			{#each benefits as benefit}
 				<li>{benefit}</li>
@@ -18,3 +18,4 @@
 		</ul>
 	</span>
 </div>
+
